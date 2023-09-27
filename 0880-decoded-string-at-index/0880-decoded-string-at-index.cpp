33bbcb4904d3d023874500f3c444ce802f055720
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-09-27 18:59:00 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (11.71%) - LeetHub

---
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    std::string decodeAtIndex(string s, int k) {
+        long long length = 0;
+        int i = 0;
+        
+        while (length < k) {
+            if (isdigit(s[i])) {
+                length *= s[i] - '0';
+                //cout<<length<<endl;
+            } else {
+                length++;
+            }
+            i++;
+        }
+        
+        for (int j = i - 1; j >= 0; j--) {
+            if (isdigit(s[j])) {
+                length /= s[j] - '0';
+                k %= length;
+            } else {
+                if (k == 0 || k == length) {
+                    return string(1, s[j]);  // Convert char to std::string
+                }
+                length--;
+            }
+        }
+        
+        return ""; // Default return, should never reach here given problem constraints
+    }
+};
