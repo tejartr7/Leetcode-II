d46b1eb0473af5ed5218b11e65ccac6fc35f44fe
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-10-23 10:28:30 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (58.82%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        while(n>0)
+        {
+            if(n==1)
+                return true;
+            if(n%4!=0)
+                return false;
+            n/=4;
+        }
+        return false;
+    }
+}
