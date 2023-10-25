752eaf0a46a955e2989f0e4456c222ab25dd971a
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-10-25 18:27:28 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (80.10%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int kthGrammar(int n, int k) {
+       if(n==1)
+           return 0;
+        if(k%2==0)
+        {
+           return kthGrammar(n-1,k/2)==1?0:1;
+        }
+        return kthGrammar(n-1,(k+1)/2);
+    }
+}
