92b46f498430acf65aaddb9e87c6a0c6f5d5485d
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-12-02 12:34:35 +0530

    Time: 5 ms (68.95%), Space: 43.5 MB (69.95%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,28 +1,33 @@
 class Solution {
-    int dp[][];
-    public int helper(int i,int j,String a,String b)
-    {
-        if(i==a.length())
-        {
-            return dp[i][j]=b.length()-j;
-        }
-        if(j==b.length())
-            return dp[i][j]=a.length()-i;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        if(a.charAt(i)==b.charAt(j))
-            return dp[i][j]=helper(i+1,j+1,a,b);
-        else{
-            int x=1+helper(i+1,j+1,a,b);
-            int y=1+helper(i,j+1,a,b);
-            int z=1+helper(i+1,j,a,b);
-            return dp[i][j]=Math.min(x,Math.min(y,z));
-        }
-    }
     public int minDistance(String word1, String word2) {
-        dp=new int[word1.length()+1][word2.length()+1];
-        for(int x[]:dp)
-            Arrays.fill(x,-1);
-        return helper(0,0,word1,word2);
+        int i,j;
+        int m=word1.length(),n=word2.length();
+        int dp[][]=new int[m+1][n+1];
+        for(i=0;i<=m;i++)
+        {
+            dp[i][0]=i;
+        }
+        for(j=0;j<=n;j++)
+        {
+            dp[0][j]=j;
+        }
+        for(i=1;i<=m;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                if(word1.charAt(i-1)==word2.charAt(j-1))
+                {
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else
+                {
+                    int x=dp[i-1][j];
+                    int y=dp[i][j-1];
+                    int z=dp[i-1][j-1];
+                    dp[i][j]=Math.min(x,Math.min(y,z))+1;
+                }
+            }
+        }
+        return dp[m][n];
     }
 }
