547a49cac0ccb49d0c4d96aa7e3b169a45827004
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-12-02 11:27:18 +0530

    Time: 9 ms (26.30%), Space: 43.6 MB (31.66%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int helper(int i,int b,int c,int p[],int dp[][][])
+    {
+        if(i==p.length) return 0;
+        if(c==0) return 0;
+        if(dp[i][b][c]!=-1) return dp[i][b][c];
+        int pro=0;
+        if(b==1)
+        {
+            pro=Math.max(-p[i]+helper(i+1,0,c,p,dp),helper(i+1,1,c,p,dp));
+        }
+        else
+        {
+            pro=Math.max(p[i]+helper(i+1,1,c-1,p,dp),helper(i+1,0,c,p,dp));
+        }
+        return dp[i][b][c]=pro;
+    }
+    public int maxProfit(int c,int[] prices) {
+        int dp[][][]=new int[prices.length][2][c+1];
+        int i,j;
+        for(i=0;i<prices.length;i++)
+        {
+            for(j=0;j<2;j++)
+            Arrays.fill(dp[i][j],-1);
+        }
+        return helper(0,1,c,prices,dp);
+    }
+}
