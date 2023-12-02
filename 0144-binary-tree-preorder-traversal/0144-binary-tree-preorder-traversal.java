1320e0a3d264a3bc5491cc85f8735e3b01940537
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-12-02 21:46:36 +0530

    Time: 1 ms (10.98%), Space: 41.1 MB (13.68%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -1,21 +1,41 @@
-
+class pair{
+    TreeNode node;
+    int num;
+    pair(TreeNode a,int b){
+        node=a;
+        num=b;
+    }
+}
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> ans=new ArrayList<>();
+        Stack<pair> stack=new Stack<>();
+        List<Integer> pre=new ArrayList<>();
+        List<Integer> in=new ArrayList<>();
+        List<Integer> post=new ArrayList<>();
         if(root==null)
-        return ans;
-        Stack<TreeNode> stack=new Stack<>();
-        stack.push(root);
-        while(!stack.isEmpty())
+        return in;
+        stack.push(new pair(root,1));
+        while(stack.size()>0)
         {
-            TreeNode temp=stack.pop();
-            ans.add(temp.val);
-            if(temp.right!=null)
-            stack.push(temp.right);
-            if(temp.left!=null)
-            stack.push(temp.left);
+            pair top=stack.pop();
+            if(top.num==1)
+            {
+                pre.add(top.node.val);
+                stack.push(new pair(top.node,top.num+1));
+                if(top.node.left!=null)
+                    stack.push(new pair(top.node.left,1));
+            }
+            else if(top.num==2)
+            {
+                in.add(top.node.val);
+                stack.push(new pair(top.node,top.num+1));
+                if(top.node.right!=null)
+                    stack.push(new pair(top.node.right,1));
+            }
+            else{
+                post.add(top.node.val);
+            }
         }
-        return ans;
+        return pre;
     }
-    
 }
