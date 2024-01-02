13090502e2009748914070bcffbe1f7fd0b748d8
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2024-01-02 10:22:18 +0530

    Time: 5 ms (31.36%), Space: 44.7 MB (26.06%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.java
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,28 +1,20 @@
 class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
         List<List<Integer>> temp=new ArrayList<>();
+        List<List<Integer>> list=new ArrayList<>();
         int i,j,n=nums.length;
-        for(i=0;i<n;i++)
-            temp.add(new ArrayList<>());
+        for(i=0;i<n;i++) temp.add(new ArrayList<>());
         Map<Integer,Integer> map=new HashMap<>();
-        for(int x:nums)
-            map.put(x,map.getOrDefault(x,0)+1);
+        for(int x:nums)map.put(x,map.getOrDefault(x,0)+1);
         for(int x:map.keySet())
         {
-            int idx=0;
-            int cnt=map.get(x);
-            while(cnt>0)
-            {
-                temp.get(idx).add(x);
-                cnt--;
-                idx++;
-            }
+            int idx=0,cnt=map.get(x);
+            while(cnt>idx)
+                temp.get(idx++).add(x);
         }
-        List<List<Integer>> list=new ArrayList<>();
         for(List<Integer> x:temp)
         {
-            if(x.size()==0)
-                return list;
+            if(x.size()==0)return list;
             list.add(new ArrayList<>(x));
         }
         return list;
