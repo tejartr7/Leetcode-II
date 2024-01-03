6ce8fd97b1d94decc2bea7ce82c62c0b2b21ea6e
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2024-01-03 19:58:55 +0530

    Time: 11 ms (92.06%), Space: 45.1 MB (15.28%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -1,15 +1,12 @@
 class Solution {
     public int numberOfBeams(String[] bank) {
-        int cnt=0,prev=0,ans=0,val=0;
+        int cnt=0,prev=0,ans=0;
         int i,j,m=bank.length,n=bank[0].length();
         for(i=0;i<m;i++)
         {
             int temp=0;
             for(j=0;j<n;j++){
-                if(bank[i].charAt(j)=='1')
-                {
-                    temp++;
-                }
+                if(bank[i].charAt(j)=='1') temp++;
             }
             if(temp>0)
             {
