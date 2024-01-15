4400424153b42b2b8fe04205cf392b748bb18ee7
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2024-01-15 20:19:51 +0530

    Time: 90 ms (42.87%), Space: 111.4 MB (6.34%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -1,30 +1,35 @@
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
-        int i,j,n=matches.length;
-        List<List<Integer>> list=new ArrayList<>();
-        List<Integer> ans1=new ArrayList<>();
-        List<Integer> ans2=new ArrayList<>();
-        TreeMap<Integer,Integer> map1=new TreeMap<>();
-        TreeMap<Integer,Integer> map2=new TreeMap<>();
-        for(int x[]:matches)
-        {
-            int a=x[0];
-            int b=x[1];
-            map1.put(a,map1.getOrDefault(a,0)+1);
-            map2.put(b,map2.getOrDefault(b,0)+1);
+        int n = matches.length;
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> ans1 = new ArrayList<>();
+        List<Integer> ans2 = new ArrayList<>();
+        Map<Integer, Integer> map1 = new HashMap<>();
+        Map<Integer, Integer> map2 = new HashMap<>();
+
+        for (int[] x : matches) {
+            int a = x[0];
+            int b = x[1];
+            map1.put(a, map1.getOrDefault(a, 0) + 1);
+            map2.put(b, map2.getOrDefault(b, 0) + 1);
         }
-        for(int x:map1.keySet())
-        {
-            if(!map2.containsKey(x))
-            {
+
+        for (int x : map1.keySet()) {
+            if (!map2.containsKey(x)) {
                 ans1.add(x);
             }
         }
-        for(int x:map2.keySet())
-        {
-            if(map2.get(x)==1)
+
+        for (int x : map2.keySet()) {
+            if (map2.get(x) == 1) {
                 ans2.add(x);
+            }
         }
+
+        // Sort ans1 and ans2
+        Collections.sort(ans1);
+        Collections.sort(ans2);
+
         list.add(ans1);
         list.add(ans2);
         return list;
