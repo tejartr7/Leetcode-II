623d28c8484c18bb8a2013d07f5a8b229a002e30
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2024-01-19 13:32:59 +0530

    Time: 7 ms (54.05%), Space: 45.2 MB (32.98%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,32 +1,28 @@
 class Solution {
-    public boolean valid(int i,int j,int m,int n)
-    {
-        return i>=0 && i<m && j>=0 && j<n;
-    }
     public int minFallingPathSum(int[][] matrix) {
-        int i,j,m=matrix.length,n=matrix[0].length;
-        int ans=Integer.MAX_VALUE;
-        if(m==1){
-        for(int x:matrix[0])
-            ans=Math.min(ans,x);
-        return ans;
-        }
-        for(i=1;i<m;i++)
-        {
-            for(j=0;j<n;j++)
-            {
-                int temp=Integer.MAX_VALUE;
-                if(valid(i-1,j,m,n))
-                    temp=Math.min(temp,matrix[i-1][j]);
-                if(valid(i-1,j+1,m,n))
-                    temp=Math.min(temp,matrix[i-1][j+1]);
-                if(valid(i-1,j-1,m,n))
-                    temp=Math.min(temp,matrix[i-1][j-1]);
-                matrix[i][j]+=temp;
-                if(i==m-1)
-                    ans=Math.min(ans,matrix[i][j]);
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int minPrev = matrix[i - 1][j];
+
+                if (j > 0) {
+                    minPrev = Math.min(minPrev, matrix[i - 1][j - 1]);
+                }
+                if (j < n - 1) {
+                    minPrev = Math.min(minPrev, matrix[i - 1][j + 1]);
+                }
+
+                matrix[i][j] += minPrev;
             }
         }
+
+        int ans = Integer.MAX_VALUE;
+        for (int x : matrix[m - 1]) {
+            ans = Math.min(ans, x);
+        }
+
         return ans;
     }
 }
