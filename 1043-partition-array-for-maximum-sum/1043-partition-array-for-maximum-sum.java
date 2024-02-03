7718ebc5bd170e9da065f9a3b82b73a98606f882
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2024-02-03 19:58:34 +0530

    Time: 96 ms (5.39%), Space: 45.4 MB (5.65%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -1,33 +1,52 @@
 class Solution {
     int dp[][];
-    public int helper(int prev,int idx,int[] arr,int k)
-    {
-        int i,j,n=arr.length;
-        if(idx>=n)
+    
+    public int helper(int prev, int idx, int[] arr, int k) {
+        int n = arr.length;
+        
+        if (idx >= n)
             return 0;
-        int not=0,take=0;
-        if(dp[prev][idx]!=-1)
+
+        if (dp[prev][idx] != -1)
             return dp[prev][idx];
-        if(idx-prev+1==k)
-        {
-            int maxi=0;
-            for(i=prev;i<=idx;i++)
-                maxi=Math.max(maxi,arr[i]);
-            take=maxi*k+helper(idx+1,idx+1,arr,k);
+
+        int not = 0, take = 0;
+
+        if (idx - prev + 1 == k) {
+            int maxi = 0;
+
+            // Find the maximum element in the current partition
+            for (int i = prev; i <= idx; i++)
+                maxi = Math.max(maxi, arr[i]);
+
+            // Calculate the sum by taking the maximum element in the partition multiplied by k
+            take = maxi * k + helper(idx + 1, idx + 1, arr, k);
+        } else if (idx - prev + 1 < k) {
+            int maxi = 0;
+
+            // Find the maximum element in the current partition
+            for (int i = prev; i <= idx; i++)
+                maxi = Math.max(maxi, arr[i]);
+
+            // Calculate the sum by taking the maximum element in the partition multiplied by the partition size
+            take = maxi * (idx - prev + 1) + helper(idx + 1, idx + 1, arr, k);
+
+            // Calculate the sum without taking the current partition
+            not = helper(prev, idx + 1, arr, k);
         }
-        else if(idx-prev+1<k){
-            int maxi=0;
-            for(i=prev;i<=idx;i++)
-                maxi=Math.max(maxi,arr[i]);
-            take=(maxi*(idx-prev+1))+helper(idx+1,idx+1,arr,k);
-            not=helper(prev,idx+1,arr,k);
-        }
-        return dp[prev][idx]=Math.max(take,not);
+
+        // Return the maximum sum between including and excluding the current partition
+        return dp[prev][idx] = Math.max(take, not);
     }
+
     public int maxSumAfterPartitioning(int[] arr, int k) {
-        dp=new int[501][501];
-        for(int x[]:dp)
-            Arrays.fill(x,-1);
-        return helper(0,0,arr,k);
+        int n = arr.length;
+        dp = new int[n][n];
+        
+        // Initialize dp array with -1
+        for (int x[] : dp)
+            Arrays.fill(x, -1);
+
+        return helper(0, 0, arr, k);
     }
 }
