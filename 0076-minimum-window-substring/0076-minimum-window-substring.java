c02559c125b2abb366d5e21cb29ff8869eeb93b5
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2024-02-04 11:32:33 +0530

    Time: 68 ms (19.70%), Space: 45.5 MB (20.70%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,24 +1,22 @@
-import java.util.*;
 
 class Solution {
     public String minWindow(String s, String t) {
-        Map<Character, Integer> map1 = new HashMap<>();
-        Map<Character, Integer> map2 = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
         int i, j = 0, m = s.length(), n = t.length();
         int need = 0;
         int len = 10000000;
         String ans = "";
 
         for (i = 0; i < n; i++) {
-            map1.put(t.charAt(i), map1.getOrDefault(t.charAt(i), 0) + 1);
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) + 1);
         }
-        int cnt=map1.size();
+        int cnt=map.size();
         for (i = 0; i < m; i++) {
             char ch=s.charAt(i);
-            if(map1.containsKey(ch))
+            if(map.containsKey(ch))
             {
-                map1.put(ch,map1.get(ch)-1);
-                if(map1.get(ch)==0)
+                map.put(ch,map.get(ch)-1);
+                if(map.get(ch)==0)
                     cnt--;
             }
             while(j<m && cnt==0)
@@ -29,10 +27,10 @@ class Solution {
                     ans=s.substring(j,i+1);
                 }
                 char x=s.charAt(j);
-                if(map1.containsKey(x))
+                if(map.containsKey(x))
                 {
-                    map1.put(x,map1.get(x)+1);
-                    if(map1.get(x)>0)
+                    map.put(x,map.get(x)+1);
+                    if(map.get(x)>0)
                         cnt++;
                 }
                 j++;
