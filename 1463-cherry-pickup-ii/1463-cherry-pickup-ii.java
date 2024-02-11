4587597c6d00f8c812bd941c28c6beaabf2b1350
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2024-02-11 18:23:50 +0530

    Time: 49 ms (15.67%), Space: 53.4 MB (5.08%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -1,48 +1,48 @@
 class Solution {
-    public int cherryPickup(int[][] grid) {
-        int dir[] = new int[]{-1, 0, 1};
-
-        int row = grid.length;
-        int col = grid[0].length;
-        int dp[][][] = new int[row][col][col]; 
-        
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
-                for(int k = 0; k < col; k++){
-                    dp[i][j][k] = -1;
-                }
-            }
-        }
-        int col1 = 0; 
-        int col2 = col - 1; 
-        
-        dp[0][col1][col2] = grid[0][col1] + grid[0][col2]; 
-        int max = dp[0][col1][col2]; 
-        
-        for(int i = 1; i < row; i++){ 
-            for(int c1 = 0; c1 < col; c1++){ 
-                for(int c2 = 0; c2 < col; c2++){
-                    int prev = dp[i - 1][c1][c2]; 
-                    if(prev >= 0){ 
-                        for(int d1: dir){ 
-                            col1 = d1 + c1;
-                            for(int d2: dir){ 
-                                col2 = d2 + c2;
-                                if(inRange(col1, col) && inRange(col2, col)){
-                                    dp[i][col1][col2] = Math.max(dp[i][col1][col2], prev+(col1 == col2 ? grid[i][col1] : (grid[i][col1] + grid[i][col2]))); 
-                                    max = Math.max(max, dp[i][col1][col2]); 
-                                }
-                            }
-                        }
-                    }
-                    
-                }
-            }
-        }
-        return max;
+    int dp[][][];
+    public boolean valid(int i,int j,int m,int n)
+    {
+        return i>=0 && i<m && j>=0 && j<n;
     }
-
-    public boolean inRange(int val, int limit){
-        return 0 <= val && val < limit;
+    public int helper(int i,int j1,int j2,int[][] grid)
+    {
+        int m=grid.length,n=grid[0].length;
+        if(!valid(i,j1,m,n) || !valid(i,j2,m,n))
+            return -1000_000_000;
+        if(i==m-1)
+        {
+            if(j1==j2)
+                return grid[i][j1];
+            return grid[i][j1]+grid[i][j2];
+        }
+        if(dp[i][j1][j2]!=-1)
+            return dp[i][j1][j2];
+        int maxi=0;
+        int dirs[]={-1,0,1};
+        for(int x:dirs)
+        {
+            for(int y:dirs)
+            {
+                if(j1==j2)
+                {
+                    maxi=Math.max(maxi,grid[i][j1]+helper(i+1,j1+x,j2+y,grid));
+                }
+                else
+                {
+                    maxi=Math.max(maxi,grid[i][j1]+grid[i][j2]+helper(i+1,j1+x,j2+y,grid));
+                }
+            }
+        }
+        return dp[i][j1][j2]=maxi;
+    }
+    public int cherryPickup(int[][] grid) {
+        dp=new int[71][71][71];
+        for(int x[][]:dp)
+        {
+            for(int y[]:x){
+                Arrays.fill(y,-1);
+            }
+        }
+        return helper(0,0,grid[0].length-1,grid);
     }
 }
