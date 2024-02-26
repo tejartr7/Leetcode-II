70cf3d6cfb280da13d6462ba3fd404aa0dba7069
RTR <97605400+tejartr7@users.noreply.github.com>
2024-02-26 19:48:35 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (82.26%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null)
+            return q==null;
+        if(q==null)
+            return p==null;
+        if(p.val!=q.val)
+            return false;
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
+}
