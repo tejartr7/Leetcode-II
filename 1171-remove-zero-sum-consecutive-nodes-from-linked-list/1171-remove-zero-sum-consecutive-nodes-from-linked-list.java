eff915d24751191eed56db48b87e7386114b4d63
RTR 🚀 <97605400+tejartr7@users.noreply.github.com>
2024-03-12 21:00:53 +0530

    Time: 2 ms (89.13%), Space: 43.3 MB (80.43%) - LeetHub

---
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.java
@@ -0,0 +1,25 @@
+class Solution {
+    public ListNode removeZeroSumSublists(ListNode head) {
+        ListNode front = new ListNode(0, head);
+        ListNode start = front;
+
+        while (start != null) {
+            int prefixSum = 0;
+            ListNode end = start.next;
+
+            while (end != null) {
+                // Add end's value to the prefix sum
+                prefixSum += end.val;
+                // Delete zero sum consecutive sequence 
+                // by setting node before sequence to node after
+                if (prefixSum == 0) {
+                    start.next = end.next;
+                }
+                end = end.next;
+            }
+
+            start = start.next;
+        }
+        return front.next;
+    }
+}
