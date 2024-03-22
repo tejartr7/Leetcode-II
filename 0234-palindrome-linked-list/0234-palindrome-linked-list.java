874eaf5172f712e7c2e3ab025600d6ca6d0513c1
RTR 🚀 <97605400+tejartr7@users.noreply.github.com>
2024-03-22 21:29:30 +0530

    Time: 6 ms (49.88%), Space: 68.1 MB (53.93%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,41 @@
+class Solution {
+   private ListNode reverse(ListNode a)
+    { 
+        ListNode curr=a;
+        ListNode temp=null;
+        ListNode prev=null;
+        while(curr!=null)
+        {
+            temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+     return prev;
+    }
+    private ListNode mid(ListNode k)
+    {
+        ListNode a=k;
+        ListNode b=k;
+        while( a!=null &&a.next!=null)
+        {
+            a=a.next.next;
+            b=b.next;
+        }
+        return b;
+    }
+    public boolean isPalindrome(ListNode head) {
+        ListNode b=head;
+        ListNode curr=mid(b);
+        ListNode p=reverse(curr);
+        ListNode a=head;
+        while(p!=null)
+        {
+            if(p.val!=a.val)
+                return false;
+            p=p.next;
+            a=a.next;
+        }
+        return true;
+    }
+}
