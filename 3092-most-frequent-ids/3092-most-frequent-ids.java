480461139366e86dc2de60f6bc766aa596396e7d
RTR 🚀 <97605400+tejartr7@users.noreply.github.com>
2024-03-24 10:12:24 +0530

    Time: 273 ms (25.00%), Space: 63.5 MB (100.00%) - LeetHub

---
+++ 3092-most-frequent-ids/3092-most-frequent-ids.java
@@ -0,0 +1,49 @@
+class pair{
+    long x,y;
+    pair(long a,long b){
+        x=a;
+        y=b;
+    }
+}
+class Solution {
+    public long[] mostFrequentIDs(int[] nums, int[] freq) {
+        Map<Long,Long> map=new HashMap<>();
+       TreeMap<Long, Long> tmap = new TreeMap<>(Collections.reverseOrder());
+        int i,j,n=nums.length;
+        long ans[]=new long[n];
+        for(i=0;i<n;i++)
+        {
+            long x=nums[i];
+            long y=freq[i];
+            
+            if(y>0)
+            {
+                if(!map.containsKey(x)){
+                map.put(x,y);
+                tmap.put(y,tmap.getOrDefault(y,0l)+1l);
+                }
+                else{
+                    long temp=map.get(x);
+                    long key=temp+freq[i];
+                    tmap.put(temp,tmap.getOrDefault(temp,0l)-1l);
+                    if(tmap.get(temp)==0l) tmap.remove(temp);
+                    tmap.put(key,tmap.getOrDefault(key,0l)+1l);
+                    map.put(x,map.get(x)+y);
+                }
+            }
+            else{
+                long temp=map.get(x);
+                long key=temp+freq[i];
+                map.put(x,map.getOrDefault(x,0l)+freq[i]);
+                //System.out.println(temp);
+                tmap.put(temp,tmap.getOrDefault(temp,0l)-1l);
+                if(tmap.get(temp)==0l) tmap.remove(temp);
+                tmap.put(key,tmap.getOrDefault(key,0l)+1l);
+            }
+            //System.out.println(map);
+            //System.out.println(tmap);
+            ans[i]=tmap.firstKey();
+        }
+        return ans;
+    }
+}
