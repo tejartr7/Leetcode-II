17d09fc761ba4a8c5e868cd0cbfc017525ed9a8d
RTR 🚀 <97605400+tejartr7@users.noreply.github.com>
2024-04-17 21:54:57 +0530

    Time: 3 ms (54.93%), Space: 44.2 MB (71.49%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.java
@@ -1,27 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    String ans;
-    public void helper(TreeNode root,String s)
+    public void helper(TreeNode root,String ans[],StringBuilder temp)
     {
         if(root==null)
         {
             return ;
         }
-        if(root.left==null && root.right==null)
+        if(root!=null && root.left==null && root.right==null)
         {
-            String temp=(char)(root.val+'a')+s;
-            if(temp.compareTo(ans)<0)
-            {
-                ans=temp;
-            }
+            temp.insert(0,(char)(root.val+97));
+            int x=ans[0].compareTo(temp.toString());
+            if(x>0)
+                ans[0]=temp.toString();
+            temp.deleteCharAt(0);
             return ;
         }
-        String temp=(char)(root.val+'a')+s;
-        helper(root.left,temp);
-        helper(root.right,temp);
+        temp.insert(0,(char)(root.val+97));
+        helper(root.left,ans,temp);
+        helper(root.right,ans,temp);
+        temp.deleteCharAt(0);
+        
     }
     public String smallestFromLeaf(TreeNode root) {
-        ans="z".repeat(10000);
-        helper(root,"");
-        return ans;
+        String ans[]=new String[1];
+        ans[0]="z".repeat(10000);
+        helper(root,ans,new StringBuilder());
+        return ans[0];
     }
 }
