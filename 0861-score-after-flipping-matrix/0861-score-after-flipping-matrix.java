5d4ffafdfff61f96c147820d6473d81b8822a671
RTR 🚀 <97605400+tejartr7@users.noreply.github.com>
2024-05-14 21:28:16 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (12.35%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.java
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.java
@@ -1,48 +1,47 @@
 class Solution {
-    public void helperR(int r, int[][] grid) {
-    int n = grid[0].length;
-    
-    for (int j = 0; j < n; j++) {
-        grid[r][j] = grid[r][j] == 1 ? 0 : 1;
-    }
-    }
-
-    public void helperC(int c, int[][] grid) {
-    int m = grid.length;
-    
-    for (int i = 0; i < m; i++) {
-        grid[i][c] = grid[i][c] == 1 ? 0 : 1;
-    }
-    }
     public int matrixScore(int[][] grid) {
-        int i,j,m=grid.length,n=grid[0].length;
-        int ans=0;
-        for(i=0;i<m;i++){
-            if(grid[i][0]==0)
-            {
-                helperR(i,grid);
-            }
-        }
-        for(j=0;j<n;j++){
-            int cnt=0;
-            for(i=0;i<m;i++){
-                if(grid[i][j]==1)
-                cnt++;
-            }
-            if(m-cnt>cnt)
-                helperC(j,grid);
-        }
-        int temp=0;
-        for(i=0;i<m;i++){
-            int y=0;
-            for(j=0;j<n;j++){
-                //System.out.print(grid[i][j]+" ");
-                if(grid[i][j]==1){
-                    temp+=(1<<(n-1-j));
+        int m = grid.length;
+        int n = grid[0].length;
+
+        // Set first column
+        for (int i = 0; i < m; i++) {
+            if (grid[i][0] == 0) {
+                // Flip row
+                for (int j = 0; j < n; j++) {
+                    grid[i][j] = 1 - grid[i][j];
                 }
             }
-            //System.out.println();
         }
-        return temp;
+
+        // Optimize columns except first column
+        for (int j = 1; j < n; j++) {
+            int countZero = 0;
+            // Count zeros
+            for(int i = 0; i < m; i++) {
+                if(grid[i][j] == 0) {
+                    countZero++;
+                }
+            }
+            // Flip the column if there are more zeros for better score
+            if(countZero > m-countZero) {
+                for(int i = 0; i < m; i++) {
+                    grid[i][j] = 1 - grid[i][j];
+                }
+            }
+        }
+
+        // Calculate the final score considering bit positions
+        int score = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // Left shift bit by place value of column to find column contribution
+                int columnScore = grid[i][j] << (n - j - 1);
+                // Add contribution to score
+                score += columnScore;
+            }
+        }
+
+        // return final result
+        return score;
     }
 }
