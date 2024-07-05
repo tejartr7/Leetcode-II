145a941f142ff1ce37fd1d567aa2843684644a34
RTR 🚀 <97605400+tejartr7@users.noreply.github.com>
2024-07-05 20:21:18 +0530

    Time: 4 ms (100.00%), Space: 60.5 MB (64.65%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -1,46 +1,26 @@
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        int i,j;
-        int ans[]=new int[2];
-        int cnt=1,p=-1;
-        int f=-1;
-        ans[0]=1000_000_000;
-        ans[1]=0;
-        ListNode prev=head;
-        ListNode curr=head.next;
-        if(head==null || head.next==null ||curr.next==null)
-            return new int[]{-1,-1};
-        while(curr!=null){
-            if(curr.val>prev.val && curr.next!=null){
-                if(curr.next.val<curr.val){
-                    if(f==-1){
-                        f=cnt;
-                    }
-                    if(p!=-1)
-                    ans[0]=Math.min(ans[0],cnt-p);
-                    if(f!=-1)
-                        ans[1]=Math.max(ans[1],cnt-f);
-                    p=cnt;
-                }
+        int[] ans = new int[]{-1, -1};
+        if (head == null || head.next == null || head.next.next == null) return ans;
+
+        int cnt = 1, p = -1, f = -1, minDist = Integer.MAX_VALUE;
+        ListNode prev = head, curr = head.next;
+
+        while (curr.next != null) {
+            if ((curr.val > prev.val && curr.val > curr.next.val) || (curr.val < prev.val && curr.val < curr.next.val)) {
+                if (f == -1) f = cnt;
+                if (p != -1) minDist = Math.min(minDist, cnt - p);
+                p = cnt;
             }
-            else if(curr.val<prev.val && curr.next!=null){
-                if(curr.next.val>curr.val){
-                    if(f==-1){
-                        f=cnt;
-                    }
-                    if(p!=-1)
-                        ans[0]=Math.min(ans[0],cnt-p);
-                    if(f!=-1)
-                        ans[1]=Math.max(ans[1],cnt-f);
-                    p=cnt;
-                }
-            }
-            prev=curr;
-            curr=curr.next;
+            prev = curr;
+            curr = curr.next;
             cnt++;
         }
-        ans[0]=ans[0]==1000_000_000?-1:ans[0];
-        ans[1]=ans[1]==0?-1:ans[1];
+
+        if (p != f) {
+            ans[0] = minDist;
+            ans[1] = p - f;
+        }
         return ans;
     }
 }
