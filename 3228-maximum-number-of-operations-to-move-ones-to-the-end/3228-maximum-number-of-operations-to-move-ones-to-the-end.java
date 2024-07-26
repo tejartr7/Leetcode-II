09bc9348b67eb6050accc30f77053d792078b551
RTR 🚀 <97605400+tejartr7@users.noreply.github.com>
2024-07-26 10:46:10 +0530

    Time: 10 ms (19.70%), Space: 45.7 MB (15.27%) - LeetHub

--- 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.java
+++ 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.java
@@ -1,31 +1,31 @@
 class Solution {
     public int maxOperations(String s) {
-        int i,j,n=s.length();
-        List<Integer> list = new ArrayList<>();
-        int cnt=0,idx=-1;
-        for(i=n-1;i>=0;i--){
-            if(s.charAt(i)=='0'){
-                idx=i;
-                break;
-            }
-        }
-        if(i<=0) return 0;
-        for(i=0;i<=idx;i++){
-            char ch=s.charAt(i);
-            if(ch=='0'){
-                if(cnt>0){
-                    list.add(cnt);
-                    cnt=0;
+        int n = s.length();
+        int lastZeroIndex = s.lastIndexOf('0');
+        if (lastZeroIndex == -1) return 0;
+
+        int count = 0;
+        List<Integer> gaps = new ArrayList<>();
+
+        for (int i = 0; i <= lastZeroIndex; i++) {
+            char ch = s.charAt(i);
+            if (ch == '0') {
+                if (count > 0) {
+                    gaps.add(count);
+                    count = 0;
                 }
+            } else {
+                count++;
             }
-            else cnt++;
         }
-        int ans=0;
-        int temp=0;
-        for(int x:list){
-            temp+=x;
-            ans+=temp;
+
+        int result = 0;
+        int cumulativeSum = 0;
+        for (int gap : gaps) {
+            cumulativeSum += gap;
+            result += cumulativeSum;
         }
-        return ans;
+
+        return result;
     }
 }
