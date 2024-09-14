063e5ab9db09d0c28980e965f1a2527dfe37fbb0
RTR 🚀 <97605400+tejartr7@users.noreply.github.com>
2024-09-14 16:09:33 +0530

    Time: 4918 ms (23.63%), Space: 45.9 MB (51.16%) - LeetHub

---
+++ 3267-count-almost-equal-pairs-ii/3267-count-almost-equal-pairs-ii.java
@@ -0,0 +1,61 @@
+class Solution { 
+    public int countPairs(int[] nums) {
+        int i, j, n = nums.length;
+        int cnt = 0;
+        for (i = 0; i < n; i++) {
+            for (j = i + 1; j < n; j++) {
+                if (i != j && valid(nums[i], nums[j])) {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+
+    public boolean valid(int x, int y) {
+        int a[] = helper(x);
+        int b[] = helper(y);
+        int diff[] = new int[4];
+        int idx = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < 8; i++) {
+            if (a[i] != b[i]) {
+                cnt++;
+                if (cnt > 4) return false; // More than 4 differences, not valid
+                diff[idx++] = i;
+            }
+        }
+
+        if (cnt == 0) {
+            return true; // No differences, they are identical
+        } else if (cnt == 2) {
+            // Two mismatches, check if they are swapped
+            return a[diff[0]] == b[diff[1]] && a[diff[1]] == b[diff[0]];
+        } else if (cnt == 3) {
+            // Three mismatches, check if they can be cyclically rotated
+            int[] p = {a[diff[0]], a[diff[1]], a[diff[2]]};
+            int[] q = {b[diff[0]], b[diff[1]], b[diff[2]]};
+            Arrays.sort(p);
+            Arrays.sort(q);
+            return Arrays.equals(p, q);
+        } else if (cnt == 4) {
+            return (a[diff[0]] == b[diff[1]] && a[diff[1]] == b[diff[0]] && a[diff[2]] == b[diff[3]] && a[diff[3]] == b[diff[2]]) ||
+                   (a[diff[0]] == b[diff[2]] && a[diff[2]] == b[diff[0]] && a[diff[1]] == b[diff[3]] && a[diff[3]] == b[diff[1]]) ||
+                   (a[diff[0]] == b[diff[3]] && a[diff[3]] == b[diff[0]] && a[diff[1]] == b[diff[2]] && a[diff[2]] == b[diff[1]]);
+        }
+
+        return false;
+    }
+
+    public int[] helper(int num) {
+        int[] cnt = new int[8];
+        int i = 0;
+        while (num > 0) {
+            cnt[i] = num % 10;
+            num /= 10;
+            i++;
+        }
+        return cnt;
+    }
+}
