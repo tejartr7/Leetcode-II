45c504297725be6f511c2c5aa343363fe06c70bc
RTR 🚀 <97605400+tejartr7@users.noreply.github.com>
2024-10-11 16:21:52 +0530

    Time: 135 ms (6.43%), Space: 52.9 MB (10.53%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.java
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.java
@@ -1,47 +1,37 @@
 class Solution {
     public int smallestChair(int[][] times, int targetFriend) {
-        PriorityQueue<tuple> arrivals = new PriorityQueue<>((a, b) -> a.y - b.y);
-        PriorityQueue<pair> departures = new PriorityQueue<>((a, b) -> a.y - b.y);
-        
-        for (int i = 0; i < times.length; i++) {
-            arrivals.offer(new tuple(i, times[i][0], times[i][1]));
+        int n = times.length;
+        int[][] events = new int[2 * n][3];
+        boolean[] chairs = new boolean[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            events[2 * i] = new int[]{times[i][0], 1, i};  // arrival
+            events[2 * i + 1] = new int[]{times[i][1], -1, i}; // departure
         }
         
-        TreeSet<Integer> availableChairs = new TreeSet<>();
-        for (int i = 0; i <= times.length; i++) availableChairs.add(i);
+        Arrays.sort(events, (a, b) -> a[0] != b[0] ? a[0] - b[0] : a[1] - b[1]);
         
-        while (!arrivals.isEmpty()) {
-            while (!departures.isEmpty() && departures.peek().y <= arrivals.peek().y) {
-                pair leaveEvent = departures.poll();
-                availableChairs.add(leaveEvent.x);
+        int[] chairMap = new int[n];
+        Arrays.fill(chairMap, -1);
+
+        for (int[] event : events) {
+            int time = event[0];
+            int type = event[1];
+            int friend = event[2];
+
+            if (type == 1) {  // Arrival
+                int assignedChair = 0;
+                while (chairs[assignedChair]) assignedChair++;
+                
+                chairs[assignedChair] = true;
+                chairMap[friend] = assignedChair;
+                
+                if (friend == targetFriend) return assignedChair;
+            } else {  // Departure
+                chairs[chairMap[friend]] = false;
             }
-            
-            tuple arrivalEvent = arrivals.poll();
-            int friendIndex = arrivalEvent.x;
-            int assignedChair = availableChairs.pollFirst();
-            
-            if (friendIndex == targetFriend) return assignedChair;
-            
-            departures.offer(new pair(assignedChair, arrivalEvent.z));
         }
         
         return -1;
-    }
-}
-
-class tuple {
-    int x, y, z;
-    tuple(int a, int b, int c) {
-        x = a;
-        y = b;
-        z = c;
-    }
-}
-
-class pair {
-    int x, y;
-    pair(int a, int b) {
-        x = a;
-        y = b;
     }
 }
