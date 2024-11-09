2d8bece9b71c3ec0e15c65949f86c2a89ee48511
RTR 🚀 <97605400+tejartr7@users.noreply.github.com>
2024-11-09 10:58:33 +0530

    Time: 1 ms (87.50%), Space: 40.4 MB (93.27%) - LeetHub

--- 3133-minimum-array-end/3133-minimum-array-end.java
+++ 3133-minimum-array-end/3133-minimum-array-end.java
@@ -1,16 +1,13 @@
 class Solution {
     public long minEnd(int n, long x) {
-        long ans = x;
-        if (n == 1) return x;
-        n -= 1;
-        for (int i = 0; i < 64; i++) {
-            if (n <= 0)
-                break;
-            long k = (x >> i) % 2;
-            if (k != 1) {
-                long t = n % 2;
-                ans |= (t << i);
-                n /= 2;
+        long ans=x;
+        if(n==1) return x;
+        n--;
+        for(int i=0;i<64;i++){
+            if((x>>i)%2==0){
+                if((n&1)==1)
+                ans|=(1L<<i);
+                n>>=1;
             }
         }
         return ans;
