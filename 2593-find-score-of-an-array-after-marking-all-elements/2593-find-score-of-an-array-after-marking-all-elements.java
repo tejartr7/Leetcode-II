05e66aed6424755dfabce279b1b7314b912ba5cb
RTR 🚀 <97605400+tejartr7@users.noreply.github.com>
2024-12-13 19:49:19 +0530

    Time: 110 ms (77.45%), Space: 59.8 MB (51.39%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.java
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.java
@@ -1,28 +1,36 @@
-class pair{
-    int x,y;
-    pair(int a,int b){
-        x=a;
-        y=b;
-    }
-}
+import java.util.*;
+
 class Solution {
     public long findScore(int[] nums) {
-        long ans=0;
-        PriorityQueue<pair> pq=new PriorityQueue<>((a,b)->a.x!=b.x?a.x-b.x:a.y-b.y);
-        int i,j,n=nums.length;
-        for(i=0;i<n;i++){
-            pq.offer(new pair(nums[i],i));
+        int n = nums.length;
+        long ans = 0;
+
+        // Create an array of pairs (value, index) and sort it
+        int[][] pairs = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            pairs[i][0] = nums[i];
+            pairs[i][1] = i;
         }
-        Set<Integer> vis=new HashSet<>();
-        while(pq.size()>0){
-            pair top=pq.poll();
-            if(vis.contains(top.y))
-            continue;
-            ans+=top.x;
-            vis.add(top.y);
-            vis.add(top.y-1);
-            vis.add(top.y+1);
+        Arrays.sort(pairs, (a, b) -> a[0] != b[0] ? Integer.compare(a[0], b[0]) : Integer.compare(a[1], b[1]));
+
+        // Use a boolean array for visited indices
+        boolean[] visited = new boolean[n];
+
+        // Traverse sorted pairs
+        for (int[] pair : pairs) {
+            int value = pair[0];
+            int index = pair[1];
+
+            // If the index is already visited, skip it
+            if (visited[index]) continue;
+
+            // Add value to the score and mark indices as visited
+            ans += value;
+            visited[index] = true;
+            if (index > 0) visited[index - 1] = true;
+            if (index < n - 1) visited[index + 1] = true;
         }
+
         return ans;
     }
 }
