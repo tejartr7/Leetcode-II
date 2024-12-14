72c486aa3e74c0bd9e23b046651dca7aa2e4ca87
RTR 🚀 <97605400+tejartr7@users.noreply.github.com>
2024-12-14 11:07:46 +0530

    Time: 20 ms (72.22%), Space: 56.8 MB (92.96%) - LeetHub

--- 2762-continuous-subarrays/2762-continuous-subarrays.java
+++ 2762-continuous-subarrays/2762-continuous-subarrays.java
@@ -1,22 +1,36 @@
 class Solution {
     public long continuousSubarrays(int[] nums) {
-        long res=0;
-        int i=0,j=0,n=nums.length;
-        PriorityQueue<Integer> max=new PriorityQueue<Integer>(Collections.reverseOrder());
-        PriorityQueue<Integer> min=new PriorityQueue<Integer>();
-        while(j<n)
-        {
-            min.offer(nums[j]);
-            max.offer(nums[j]);
-            while(min.size()>0 && max.peek()-min.peek()>2)
-            {
-                min.remove(nums[i]);
-                max.remove(nums[i]);
+        long res = 0;
+        int i = 0, n = nums.length;
+
+        // Deques to maintain the indices of max and min values
+        Deque<Integer> maxDeque = new LinkedList<>();
+        Deque<Integer> minDeque = new LinkedList<>();
+
+        for (int j = 0; j < n; j++) {
+            // Update the maxDeque: remove elements smaller than the current number
+            while (!maxDeque.isEmpty() && nums[maxDeque.peekLast()] <= nums[j]) {
+                maxDeque.pollLast();
+            }
+            maxDeque.offerLast(j);
+
+            // Update the minDeque: remove elements larger than the current number
+            while (!minDeque.isEmpty() && nums[minDeque.peekLast()] >= nums[j]) {
+                minDeque.pollLast();
+            }
+            minDeque.offerLast(j);
+
+            // Shrink the window if the condition is violated
+            while (nums[maxDeque.peekFirst()] - nums[minDeque.peekFirst()] > 2) {
+                if (maxDeque.peekFirst() == i) maxDeque.pollFirst();
+                if (minDeque.peekFirst() == i) minDeque.pollFirst();
                 i++;
             }
-            res+=j-i+1;
-            j++;
+
+            // Count the subarrays ending at index j
+            res += j - i + 1;
         }
+
         return res;
     }
 }
