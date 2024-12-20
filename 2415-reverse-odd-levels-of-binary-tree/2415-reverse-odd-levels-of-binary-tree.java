29dda8f9c96d6cec54c6ad888a3d5bcbbf1a5353
RTR 🚀 <97605400+tejartr7@users.noreply.github.com>
2024-12-20 19:46:37 +0530

    Time: 0 ms (100.00%), Space: 48.6 MB (48.52%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
@@ -1,32 +1,22 @@
-
 class Solution {
-    public TreeNode reverseOddLevels(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<TreeNode>();
-        q.add(root);
-        int lvl=0;
-        while(!q.isEmpty()){
-            int size = q.size();
-            List<TreeNode> list=new ArrayList<>();
-            for(int i=0;i<size;i++){
-                TreeNode curr = q.poll();
-                list.add(curr);
-                if(curr.left!=null) q.add(curr.left);
-                if(curr.right!=null) q.add(curr.right);
-            }
-            if(lvl%2==1){
-                int i=0,j=list.size()-1;
-                while(i<j){
-                    int temp = list.get(i).val;
-                    TreeNode a= list.get(i);
-                    TreeNode b= list.get(j);
-                    a.val=b.val;
-                    b.val=temp;
-                    i++;
-                    j--;
-                }
-            }
-            lvl++;
+    boolean flag=false;
+    public void inorder(TreeNode left,TreeNode right,boolean flag)
+    {
+        if(left==null)
+        return ;
+        if(flag)
+        {
+            int temp=left.val;
+            left.val=right.val;
+            right.val=temp;
         }
-        return root;        
+        flag=!flag;
+        inorder(left.right,right.left,flag);
+        inorder(left.left,right.right,flag);
+    }
+    public TreeNode reverseOddLevels(TreeNode root) {
+        flag=!flag;
+        inorder(root.left,root.right,flag);
+        return root;
     }
 }
